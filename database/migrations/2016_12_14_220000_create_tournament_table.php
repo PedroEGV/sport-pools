207a29fef495e66446d1d29e9802b31cbf3ef805
ErisoHV <erika.programmer@gmail.com>
2016-12-14 18:23:33 -0400

    Basic sport, player and tournamet table migrations were added.

---
+++ database/migrations/2016_12_14_220000_create_tournament_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTournamentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tournament', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sport_id')->unsigned();
+            $table->string('name');
+            $table->dateTimeTz('start_at');
+            $table->dateTimeTz('finish_at');
+            $table->timestamps();
+
+            $table->foreign('sport_id')->references('id')->on('sport');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tournament');
+    }
+}
